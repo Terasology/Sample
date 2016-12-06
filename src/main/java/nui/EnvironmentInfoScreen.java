0fb21c65b0e7a1fc1a87d98de682f98c2c0a5ef9
Zeke <ezequielroberts01@gmail.com>
2016-12-05 19:44:40 -0800

    Commit

---
+++ src/main/java/nui/EnvironmentInfoScreen.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nui;
+
+import org.terasology.engine.Time;
+import org.terasology.registry.In;
+import org.terasology.rendering.nui.CoreScreenLayer;
+import org.terasology.rendering.nui.widgets.UIButton;
+import org.terasology.rendering.nui.widgets.UIText;
+
+import java.util.Random;
+
+/**
+ * Created by Zeke on 11/30/2016.
+ */
+public class EnvironmentInfoScreen extends CoreScreenLayer{
+    private UIText infoArea;
+    private UIButton updateInfoButton;
+
+    private UIText text2;
+    private UIButton button2;
+
+    private Random rand;
+
+    @In
+    private Time time;
+
+    @Override
+    public void initialise(){
+        infoArea = find("infoArea", UIText.class);
+        updateInfoButton = find("updateInfoButton", UIButton.class);
+
+        text2 = find("text2", UIText.class);
+        button2 = find("button2", UIButton.class);
+
+        rand = new Random();
+
+        if(updateInfoButton != null){
+            updateInfoButton.subscribe(button -> {
+                final double bytesInMegabyte = 1048576.0;
+                double memoryUsage = ((double) Runtime.getRuntime().totalMemory() - (double) Runtime.getRuntime().freeMemory()) / bytesInMegabyte;
+                infoArea.setText(String.format("Welcome to the environment info screen!%n" +
+                                "The current world has been active for %.0f (in-game) seconds.%n" +
+                                "Currently running at %.2f FPS and using %.2f MB of memory out of %.2f available.",
+                        time.getGameTime(), time.getFps(),
+                        memoryUsage, Runtime.getRuntime().maxMemory() / bytesInMegabyte));
+            });
+        }
+
+        if(button2 != null) {
+            button2.subscribe(button -> {
+                int random = rand.nextInt(10 - 1 + 1) + 1;
+                String text = "Random Number: " + random;
+                text2.setText(text);
+            });
+        }
+
+
+    }
+}
