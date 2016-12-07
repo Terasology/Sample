4ccf8961ae09309df4f3a5b3f68d6b9bad3294f4
Camellia Guan <camelliaguan3@gmail.com>
2016-12-06 21:42:18 -0500

    New Interface With Inspiring Quotes

--- src/main/java/org/terasology/nui/EnvironmentInfoScreen.java
+++ src/main/java/org/terasology/nui/EnvironmentInfoScreen.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.terasology.tutorialnui;
+package org.terasology.nui;
 
 import org.terasology.rendering.nui.CoreScreenLayer;
 
