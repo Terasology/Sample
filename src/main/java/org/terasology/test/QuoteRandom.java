bb3f3f04cc3ee259dc62f59d2c76aa6ed1ef40aa
Camellia Guan <camelliaguan3@gmail.com>
2016-12-07 20:15:53 -0500

    New Interface With Inspiring Quotes

--- src/main/java/org/terasology/test/QuoteRandom.java
+++ src/main/java/org/terasology/test/QuoteRandom.java
@@ -17,5 +17,5 @@ public class QuoteRandom extends CoreScreenLayer {
     public void initialise() {
         infoArea = find("infoArea", UIText.class);
         updateInfoButton = find("updateInfoButton", UIButton.class);
-    };
+    }
 }
