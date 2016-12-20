bc4c89fa7a01cc04548ef98ccc8df17539fb255a
TheJYKoder <thejykoder@gmail.com>
2016-12-20 02:26:47 -0500

    NeverGiveUp Interface Screen (#18)
    * Create GCITutorial.ui
    
    * Create EnvironmentInfoScreen.java
    
    * Rename GCITutorial.ui to enviornmentInfoScreen.ui
    
    * Rename enviornmentInfoScreen.ui to neverGiveUp.ui
    
    * Update and rename EnvironmentInfoScreen.java to NeverGiveUp.java
    
    * Update neverGiveUp.ui

---
+++ src/main/java/org/terasology/nui/NeverGiveUp.java
@@ -0,0 +1,25 @@
+import org.terasology.engine.Time;
+import org.terasology.registry.In;
+import org.terasology.rendering.nui.CoreScreenLayer;
+import org.terasology.rendering.nui.widgets.UIButton;
+import org.terasology.rendering.nui.widgets.UIText;
+
+public class NeverGiveUp extends CoreScreenLayer {
+    private UIText infoArea;
+    private UIButton updateInfoButton;
+
+    @In
+    private Time time;
+
+    @Override
+    public void initialise() {
+        infoArea = find("infoArea", UIText.class);
+        updateInfoButton = find("updateInfoButton", UIButton.class);
+
+        if (updateInfoButton != null) {
+            updateInfoButton.subscribe(button -> {
+                infoArea.setText(String.format("But on the other hand, please don't try anything stupid."));
+            });
+        }
+    }
+}
