27687ab6b4d85aab3608e5d5c5c29cf3f14ae941
Cervator <cervator@gmail.com>
2016-12-26 00:46:47 -0500

    Code style fixes.

--- src/main/java/org/terasology/nui/InformativeLinksScreen.java
+++ src/main/java/org/terasology/nui/InformativeLinksScreen.java
@@ -1,7 +1,21 @@
-package org.terasology.tutorialnui;
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import org.terasology.engine.Time;
-import org.terasology.registry.In;
+package org.terasology.nui;
+
 import org.terasology.rendering.nui.CoreScreenLayer;
 import org.terasology.rendering.nui.widgets.UIButton;
 import org.terasology.rendering.nui.widgets.UIText;
@@ -9,7 +23,7 @@ import org.terasology.rendering.nui.widgets.UIText;
 public class InformativeLinksScreen extends CoreScreenLayer {
     private UIText infoArea;
     private UIButton updateInfoButton;
-    private int index = 0;
+    private int index;
 
     private String[] linksList = {
             "Care to visit Terasology's main website?\nhttp://terasology.org/",
@@ -19,19 +33,16 @@ public class InformativeLinksScreen extends CoreScreenLayer {
             "Follow us on Facebook:\nhttps://www.facebook.com/Terasology/"
     };
 
-    public String getNextLink(){
+    public String getNextLink() {
 
         index++;
 
-        if(index >= linksList.length){
+        if (index >= linksList.length) {
             index = 0;
         }
 
         return linksList[index];
     }
-
-    @In
-    private Time time;
 
     @Override
     public void initialise() {
