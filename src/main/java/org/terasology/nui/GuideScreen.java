e42ede6de6598f454884a9bd065df327d9e9f5b3
Ether3alInfinity <bill.nguyen.250@gmail.com>
2017-12-10 15:34:32 -0600

    screen threw NullPointer

--- src/main/java/org/terasology/nui/GuideScreen.java
+++ src/main/java/org/terasology/nui/GuideScreen.java
@@ -6,7 +6,7 @@ import org.terasology.rendering.nui.CoreScreenLayer;
 import org.terasology.rendering.nui.widgets.UIButton;
 import org.terasology.rendering.nui.widgets.UIText;
 
-public class EnvironmentInfoScreen extends CoreScreenLayer {
+public class GuideScreen extends CoreScreenLayer {
     private UIText infoArea;
     private UIButton updateInfoButton;
 
