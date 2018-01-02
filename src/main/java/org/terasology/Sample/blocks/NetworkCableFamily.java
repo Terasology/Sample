e099a5a4b50bd64a7ab94aa90dfe77561db85052
Michael Pollind <polli104@mail.chapman.edu>
2018-01-01 19:31:58 -0800

    fixed block families

--- src/main/java/org/terasology/Sample/blocks/NetworkCableFamily.java
+++ src/main/java/org/terasology/Sample/blocks/NetworkCableFamily.java
@@ -15,27 +15,59 @@
  */
 package org.terasology.Sample.blocks;
 
+import org.terasology.math.Rotation;
 import org.terasology.math.Side;
+import org.terasology.math.SideBitFlag;
 import org.terasology.math.geom.Vector3i;
+import org.terasology.naming.Name;
+import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockBuilderHelper;
+import org.terasology.world.block.BlockUri;
+import org.terasology.world.block.family.BlockSections;
 import org.terasology.world.block.family.MultiConnectFamily;
 import org.terasology.world.block.family.RegisterBlockFamily;
+import org.terasology.world.block.family.UpdatesWithNeighboursFamily;
 import org.terasology.world.block.loader.BlockFamilyDefinition;
 import org.terasology.world.block.shapes.BlockShape;
 
 @RegisterBlockFamily("sample:NetworkCable")
-public class NetworkCableFamily extends MultiConnectFamily {
+@BlockSections({"one_connection", "line_connection", "2d_corner","3d_corner","2d_t","cross","3d_side","five_connections"})
+public class NetworkCableFamily extends MultiConnectFamily implements UpdatesWithNeighboursFamily {
+
     public NetworkCableFamily(BlockFamilyDefinition definition, BlockShape shape, BlockBuilderHelper builderHelper) {
         super(definition, shape, builderHelper);
     }
 
     public NetworkCableFamily(BlockFamilyDefinition definition, BlockBuilderHelper builderHelper) {
         super(definition, builderHelper);
+
+        BlockUri blockUri = new BlockUri(definition.getUrn());
+
+        Block block = builderHelper.constructSimpleBlock(definition);
+        block.setBlockFamily(this);
+        block.setUri(new BlockUri(blockUri,new Name(String.valueOf(0))));
+        this.blocks.put((byte) 0,block);
+
+//        this.registerBlock(blockUri, definition, builderHelper, "one_connection", (byte) 0, Rotation.allValues());
+        this.registerBlock(blockUri, definition, builderHelper, "one_connection", SideBitFlag.getSides(Side.BACK), Rotation.allValues());
+        this.registerBlock(blockUri, definition, builderHelper, "line_connection",SideBitFlag.getSides(Side.BACK, Side.FRONT), Rotation.allValues());
+        this.registerBlock(blockUri, definition, builderHelper, "2d_corner",  SideBitFlag.getSides(Side.LEFT, Side.BACK), Rotation.allValues());
+        this.registerBlock(blockUri, definition, builderHelper, "3d_corner", SideBitFlag.getSides(Side.LEFT, Side.BACK, Side.TOP), Rotation.allValues());
+        this.registerBlock(blockUri, definition, builderHelper, "2d_t", SideBitFlag.getSides(Side.LEFT, Side.BACK, Side.FRONT), Rotation.allValues());
+        this.registerBlock(blockUri, definition, builderHelper, "cross", SideBitFlag.getSides(Side.RIGHT, Side.LEFT, Side.BACK, Side.FRONT), Rotation.allValues());
+        this.registerBlock(blockUri, definition, builderHelper, "3d_side", SideBitFlag.getSides(Side.LEFT, Side.BACK, Side.FRONT, Side.TOP), Rotation.allValues());
+        this.registerBlock(blockUri, definition, builderHelper, "five_connections", SideBitFlag.getSides(Side.LEFT, Side.BACK, Side.FRONT, Side.TOP, Side.BOTTOM), Rotation.allValues());
+
     }
 
     @Override
     public byte getConnectionSides() {
         return 63;
+    }
+
+    @Override
+    public Block getArchetypeBlock() {
+        return blocks.get((byte)0);
     }
 
     @Override
