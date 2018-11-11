9cede53a010366de138fa32abe09e766f7480b41
AndyTechGuy <44126397+AndyTechGuy@users.noreply.github.com>
2018-11-10 20:47:17 -0330

    Fix package declaration in 'EnvironmentInfoScreen'

--- src/main/java/org/terasology/nui/EnvironmentInfoScreen.java
+++ src/main/java/org/terasology/nui/EnvironmentInfoScreen.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.terasology.tutorial.nui;
+package org.terasology.nui;
 
 import org.terasology.engine.Time;
 import org.terasology.registry.In;
