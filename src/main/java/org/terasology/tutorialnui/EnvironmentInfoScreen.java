f7648b51e9fc593d822a2d1ca46e4361e993c79c
Nate <nathanschnitzer587@gmail.com>
2018-11-21 21:04:12 -0500

    Create Sample UI Screen - GCI Task

---
+++ src/main/java/org/terasology/tutorialnui/EnvironmentInfoScreen.java
@@ -0,0 +1,40 @@
+package org.terasology.tutorialnui;
+
+//***************************************
+// Nate Schnitzer
+// EnviornmentInfoScreen.java
+// 11/21/18
+// Google Code In Task
+//***************************************
+
+import org.terasology.engine.Time;
+import org.terasology.registry.In;
+import org.terasology.rendering.nui.CoreScreenLayer;
+import org.terasology.rendering.nui.widgets.UIButton;
+import org.terasology.rendering.nui.widgets.UIText;
+
+public class EnvironmentInfoScreen extends CoreScreenLayer {
+    private UIText infoArea;
+    private UIButton updateInfoButton;
+
+    @In
+    private Time time;
+
+    @Override
+    public void initialise() {
+        infoArea = find("infoArea", UIText.class);
+        updateInfoButton = find("updateInfoButton", UIButton.class);
+
+        if (updateInfoButton != null) {
+            updateInfoButton.subscribe(button -> {
+                double bytesInMegabyte = 1048576.0;
+                double memoryUsage = ((double) Runtime.getRuntime().totalMemory() - (double) Runtime.getRuntime().freeMemory()) / bytesInMegabyte;
+                infoArea.setText(String.format("Welcome to the environment info screen!%n" +
+                        "The current world has been active for %.0f (in-game) seconds.%n" +
+                        "Currently running at %.2f FPS and using %.2f MB of memory out of %.2f available.",
+                    time.getGameTime(), time.getFps(),
+                    memoryUsage, Runtime.getRuntime().maxMemory() / bytesInMegabyte));
+            });
+        }
+    }
+}
