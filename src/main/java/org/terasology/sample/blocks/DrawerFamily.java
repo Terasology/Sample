2f595663a081786a20b6a33ccba488026f08932d
Tobias Nett <skaldarnar@googlemail.com>
2021-02-06 14:15:29 +0100

    feat(JOML): migrate `BlockComponent`
    Depends on MovingBlocks/Terasology#4470

--- src/main/java/org/terasology/sample/blocks/DrawerFamily.java
+++ src/main/java/org/terasology/sample/blocks/DrawerFamily.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.sample.blocks;
 
 import org.joml.Vector3ic;
@@ -74,6 +61,6 @@ public class DrawerFamily extends MultiConnectFamily {
         BlockComponent blockComponent = neighborEntity.getComponent(BlockComponent.class);
 
         return neighborEntity.hasComponent(ConnectsToDrawerComponent.class)
-            || (blockComponent != null && blockComponent.getBlock().isFullSide(connectSide));
+            || (blockComponent != null && blockComponent.block.isFullSide(connectSide));
     }
 }
